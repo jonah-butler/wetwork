886addca66b1aad7d537b2a6d1da0f44c199158e
Jonah Butler <jonahbutler6@gmail.com>
2025-12-07 09:28:08 -0500

    adding document title

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,35 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Terminal from '../views/Terminal.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import Terminal from '../views/Terminal.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: Terminal
+    component: Terminal,
+    meta: {
+      title: "wetwork",
+    }
   },
   {
     path: '/epk',
     name: 'Epk',
-     component: () => import(/* webpackChunkName: "about" */ '../views/EPK.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/EPK.vue'),
+    meta: {
+      title: "EPK"
+    },
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title as string;
+  }
+  next();
 })
 
 export default router
